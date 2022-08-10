3ab86d8676e4121eca54eeeab956a2bcf271e60f
bereszit <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>
2022-08-10 16:10:27 +0200

    Adding Dockerfile and Jenkinsfile fixed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ object Main {
   def main(args:Array[String]): Unit = {
     val a = List(A(1, "one"), A(2, "two"), A(3, "three"))
     val result = a.map(A.getIdName(_))
-    println(result)
+    println("This is the result: " + result)
   }
 
 }
